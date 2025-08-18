77936b247530da07c4d6eaa745cb23f7db017369
Philipp Flotho <PhilippFlotho@gmail.com>
2025-08-18 09:46:44 +0900

    first version of the flowregwidget

---
+++ src/napari_flowreg/__init__.py
@@ -0,0 +1,9 @@
+"""
+napari-flowreg: A napari plugin for Flow-Registration motion correction.
+"""
+
+__version__ = "0.0.1-alpha.1"
+
+from .widgets import FlowRegWidget
+
+__all__ = ["FlowRegWidget"]
